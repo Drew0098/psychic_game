c037edc3df9b69492439e0eae501e42922d6d301
Drew0098 <a.eaton0098@gmail.com>
2017-11-10 20:33:28 -0500

    done

--- assets/js/my.js
+++ assets/js/my.js
@@ -4,24 +4,43 @@ var guessesLeft = 9;
 var guessesSoFar = [];
 var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
 'u', 'v', 'w', 'x', 'y', 'z'];
-var html = "<p>Guess what letter I'm thinking of</p>" +
+var pickRandom = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+var html = 	guessesLeft = 9;
+	guessesSoFar.length = 0;
+	html = "<p>Guess what letter I'm thinking of</p>" +
 	"<p>Wins: " + wins + "</p>" +
 	"<p>losses: " + losses + "</p>" +
 	"<p>Guesses Left: " + guessesLeft + "</p>" +
 	"<p>Your Guesses so far: " + guessesSoFar.join(', ') + "</p>";
 
 	document.getElementById("game").innerHTML = html;
+
 reset();
+random();
 
 document.onkeyup = function(event) {
-	var playerGuess = String.fromCharCode(event.keycode).toLowerCase();
+	var playerGuess = event.key;
 	guessesSoFar.push(playerGuess);
 	if (playerGuess == pickRandom) {
 		wins++;
 		reset();
+		html = "<p>Guess what letter I'm thinking of</p>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>losses: " + losses + "</p>" +
+		"<p>Guesses Left: " + guessesLeft + "</p>" +
+		"<p>Your Guesses so far: " + guessesSoFar.join(', ') + "</p>";
+
+		document.getElementById("game").innerHTML = html;
 	} else if (playerGuess !== pickRandom) {
 		guessesLeft--;
-	} else (guessesLeft == 0) {
+		html = "<p>Guess what letter I'm thinking of</p>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>losses: " + losses + "</p>" +
+		"<p>Guesses Left: " + guessesLeft + "</p>" +
+		"<p>Your Guesses so far: " + guessesSoFar.join(', ') + "</p>";
+
+		document.getElementById("game").innerHTML = html;
+	} if (guessesLeft === 0) {
 		losses++;
 		reset();
 	}
@@ -29,5 +48,14 @@ document.onkeyup = function(event) {
 function reset() {
 	guessesLeft = 9;
 	guessesSoFar.length = 0;
-	var pickRandom = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+	html = "<p>Guess what letter I'm thinking of</p>" +
+	"<p>Wins: " + wins + "</p>" +
+	"<p>losses: " + losses + "</p>" +
+	"<p>Guesses Left: " + guessesLeft + "</p>" +
+	"<p>Your Guesses so far: " + guessesSoFar.join(', ') + "</p>";
+
+	document.getElementById("game").innerHTML = html;
+}
+function random() {
+	pickRandom;
 }
