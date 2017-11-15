a32349f935dea8a2598be6594ec5a8cb8240bb91
Drew0098 <a.eaton0098@gmail.com>
2017-11-14 21:00:08 -0500

    index.html

--- assets/js/my.js
+++ assets/js/my.js
@@ -5,9 +5,7 @@ var guessesSoFar = [];
 var randomLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
 'u', 'v', 'w', 'x', 'y', 'z'];
 var pickRandom = randomLetter[Math.floor(Math.random() * randomLetter.length)];
-var html = 	guessesLeft = 9;
-	guessesSoFar.length = 0;
-	html = "<p>Guess what letter I'm thinking of</p>" +
+var html = "<p>Guess what letter I'm thinking of</p>" +
 	"<p>Wins: " + wins + "</p>" +
 	"<p>losses: " + losses + "</p>" +
 	"<p>Guesses Left: " + guessesLeft + "</p>" +
@@ -22,13 +20,14 @@ document.onkeyup = function(event) {
 	var playerGuess = event.key;
 	guessesSoFar.push(playerGuess);
 	if (playerGuess == pickRandom) {
+		alert("Congratulations!!! You Win!!!");
 		wins++;
 		reset();
 		html = "<p>Guess what letter I'm thinking of</p>" +
 		"<p>Wins: " + wins + "</p>" +
 		"<p>losses: " + losses + "</p>" +
 		"<p>Guesses Left: " + guessesLeft + "</p>" +
-		"<p>Your Guesses so far: " + guessesSoFar.join(', ') + "</p>";
+		"<p>Your Guesses so far: " + guessesSoFar.join(', ') + "</p>";		
 
 		document.getElementById("game").innerHTML = html;
 	} else if (playerGuess !== pickRandom) {
@@ -41,6 +40,7 @@ document.onkeyup = function(event) {
 
 		document.getElementById("game").innerHTML = html;
 	} if (guessesLeft === 0) {
+		confirm("Would you like to try again?");
 		losses++;
 		reset();
 	}
